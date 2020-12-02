fdff153002d8fc4ba2f5d40d016dc302f0dc92d3
Henry Post <HenryFBP@gmail.com>
2020-12-01 18:40:45 -0600

    add app and query ip on tcp

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-https://pythonawesome.com/a-python-library-for-creating-all-ascii-cui-tui-interfaces-with-widgets/
+<https://pythonawesome.com/a-python-library-for-creating-all-ascii-cui-tui-interfaces-with-widgets/>
+
+<https://github.com/jwlodek/py_cui_2048/>
+
+## Building
+
+1.  Populate `config.json`.
---
+++ app.py
@@ -0,0 +1,25 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import py_cui
+import json
+
+with open('ip', 'r') as f:
+    data = json.load(f)
+
+IP = data['ip']
+PORT = data['port']
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((IP, PORT))  # The address of the TCP server listening
+
+
+print(f"Connected to {IP[-3:]}:{PORT}")
+
+if __name__ == "__main__":
+    root = py_cui.PyCUI(7, 9)
+
+    label = root.add_label('Label Text', 0, 0)
+    button = root.add_button(
+        'Button Text', 1, 2, column_span=2, command=lambda: print("lol hi"))
+
+    root.start()
